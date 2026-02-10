d4bc9fffd56bf76d72cf2f77aa97227b644b81ef
iamtony.ca <iamtony.ca@gmail.com>
2026-02-11 00:02:12 +0900

    update

--- nav_safety_manager/setup.py
+++ nav_safety_manager/setup.py
@@ -4,12 +4,13 @@ from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'nav_safety_manager'
-submodules = "nav_safety_manager/submodules"
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name, submodules],
+    # [수정 포인트] 패키지를 자동으로 찾게 설정 (test 폴더 제외)
+    # 서브모듈이 있든 없든 알아서 처리해줍니다.
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -35,6 +36,53 @@ setup(
         ],
     },
 )
+
+
+
+
+
+
+
+
+
+
+# import os
+# from glob import glob
+# from setuptools import find_packages
+# from setuptools import setup
+
+# package_name = 'nav_safety_manager'
+# submodules = "nav_safety_manager/submodules"
+
+# setup(
+#     name=package_name,
+#     version='0.0.1',
+#     packages=[package_name, submodules],
+#     data_files=[
+#         ('share/ament_index/resource_index/packages',
+#             ['resource/' + package_name]),
+#         ('share/' + package_name, ['package.xml']),
+#         # Launch files
+#         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+#         # Config files (YAML)
+#         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),        
+#     ],
+#     install_requires=['setuptools'],
+#     zip_safe=True,
+#     maintainer='User',
+#     maintainer_email='User@todo.todo',
+#     description='Navigation Safety Manager handling PLC and Collision states',
+#     license='TODO: License declaration',
+#     tests_require=['pytest'],
+#     entry_points={
+#         'console_scripts': [
+#             # 'nav_safety_manager_controller = nav_safety_manager.nav_safety_manager_controller:main',
+#             # 'nav_safety_manager_driver = nav_safety_manager.nav_safety_manager_driver:main',
+#             # node_executable_name = package_name.python_file:main_func
+#             'nav_safety_manager_node = nav_safety_manager.nav_safety_manager_node:main',            
+#         ],
+#     },
+# )
 
 
 
