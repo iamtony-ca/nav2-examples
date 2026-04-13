78405b034fa13b1c9939e1d2924e24feb1eff417
iamtony-ca <iamtony.ca@gmail.com>
2026-04-13 23:48:14 +0900

    Update run_bringup.launch.py

--- navigation2/nav2_bringup/launch/run_bringup.launch.py
+++ navigation2/nav2_bringup/launch/run_bringup.launch.py
@@ -1,5 +1,7 @@
 import os
 import yaml
+import json  # [추가]
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
@@ -7,22 +9,21 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
-    bringup_dir = get_package_share_directory('nav2_bringup') # nav2_bringup 패키지 경로 가져오기
-    # [수정됨] 새 패키지 경로 가져오기
-    fleet_config_dir = get_package_share_directory('ammr_common_config')
+    bringup_dir = get_package_share_directory('amr_bringup')
+    fleet_config_dir = get_package_share_directory('amr_fleet_config')
     
-    # [수정됨] 새 패키지에서 common_ammr.yaml 읽기
-    with open(os.path.join(fleet_config_dir, 'config', 'common_ammr.yaml'), 'r') as f:
+    with open(os.path.join(fleet_config_dir, 'config', 'common_fleet.yaml'), 'r') as f:
         common_data = yaml.safe_load(f)['common_settings']
 
+    # [핵심 수정] 문자열 캐스팅
     fleet_rewrites = {
-        'self_machine_id': common_data['machine_id'],
-        'self_type_id': common_data['type_id'],
-        'robot_ids': common_data['robot_ids']
+        'self_machine_id': str(common_data['machine_id']),
+        'self_type_id': str(common_data['type_id']),
+        'robot_ids': json.dumps(common_data['robot_ids'])
     }
     for bot_id in common_data['robot_ids']:
         if bot_id in common_data:
-            fleet_rewrites[bot_id] = common_data[bot_id]
+            fleet_rewrites[bot_id] = json.dumps(common_data[bot_id])
 
     configured_params = RewrittenYaml(
         source_file=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
